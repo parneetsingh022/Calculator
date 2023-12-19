23602a0ed485bc2cbc99a3418deb9c68b77d5da9
Harmanpreet <malsian.harmanpreet@gmail.com>
2023-12-19 11:43:56 -0800

    Added button

--- components/TextField.java
+++ components/TextField.java
@@ -5,22 +5,22 @@ import java.awt.Color;
 import javax.swing.JTextField;
 
 public class TextField {
-    private int width,height;
-    private int x,y; // location on window
+    private int width, height;
+    private int x, y; // location on window
     private Color backgroundColor;
     private JTextField textField;
-    
-    public TextField(int x, int y, int width){
+
+    public TextField(int x, int y, int width) {
         textField = new JTextField();
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = 50;
 
-        textField.setBounds(x,y,width,height);
+        textField.setBounds(x, y, width, height);
     }
 
-    public JTextField getJTextField(){
+    public JTextField getJTextField() {
         return textField;
     }
 }
